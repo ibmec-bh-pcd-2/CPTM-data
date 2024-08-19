a4c36ac415755e513699b03879c1fa3a39e6e629
Tamara Simões <94468072+tamarasimoes@users.noreply.github.com>
2024-08-19 11:36:42 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,353 @@
+{
+  "Metrô São Paulo": {
+    "Linhas": [
+      {
+        "linha": "Linha 1 - Azul",
+        "estações": [
+          {
+            "nome": "Jabaquara",
+            "conexões": []
+          },
+          {
+            "nome": "Conceição",
+            "conexões": []
+          },
+          {
+            "nome": "São Judas",
+            "conexões": []
+          },
+          {
+            "nome": "Saúde",
+            "conexões": []
+          },
+          {
+            "nome": "Praça da Árvore",
+            "conexões": []
+          },
+          {
+            "nome": "Santa Cruz",
+            "conexões": ["Linha 5 - Lilás"]
+          },
+          {
+            "nome": "Vila Mariana",
+            "conexões": []
+          },
+          {
+            "nome": "Ana Rosa",
+            "conexões": ["Linha 2 - Verde"]
+          },
+          {
+            "nome": "Paraíso",
+            "conexões": ["Linha 2 - Verde"]
+          },
+          {
+            "nome": "Vergueiro",
+            "conexões": []
+          },
+          {
+            "nome": "São Joaquim",
+            "conexões": []
+          },
+          {
+            "nome": "Liberdade",
+            "conexões": []
+          },
+          {
+            "nome": "Sé",
+            "conexões": ["Linha 3 - Vermelha"]
+          },
+          {
+            "nome": "São Bento",
+            "conexões": []
+          },
+          {
+            "nome": "Luz",
+            "conexões": ["Linha 4 - Amarela", "Linha 7 - Rubi", "Linha 11 - Coral"]
+          },
+          {
+            "nome": "Tiradentes",
+            "conexões": []
+          },
+          {
+            "nome": "Armênia",
+            "conexões": []
+          },
+          {
+            "nome": "Portuguesa-Tietê",
+            "conexões": []
+          },
+          {
+            "nome": "Carandiru",
+            "conexões": []
+          },
+          {
+            "nome": "Santana",
+            "conexões": []
+          },
+          {
+            "nome": "Jardim São Paulo-Ayrton Senna",
+            "conexões": []
+          },
+          {
+            "nome": "Parada Inglesa",
+            "conexões": []
+          },
+          {
+            "nome": "Tucuruvi",
+            "conexões": []
+          }
+        ]
+      },
+      {
+        "linha": "Linha 2 - Verde",
+        "estações": [
+          {
+            "nome": "Vila Madalena",
+            "conexões": []
+          },
+          {
+            "nome": "Sumaré",
+            "conexões": []
+          },
+          {
+            "nome": "Clínicas",
+            "conexões": []
+          },
+          {
+            "nome": "Consolação",
+            "conexões": ["Linha 4 - Amarela"]
+          },
+          {
+            "nome": "Trianon-Masp",
+            "conexões": []
+          },
+          {
+            "nome": "Brigadeiro",
+            "conexões": []
+          },
+          {
+            "nome": "Paraíso",
+            "conexões": ["Linha 1 - Azul"]
+          },
+          {
+            "nome": "Ana Rosa",
+            "conexões": ["Linha 1 - Azul"]
+          },
+          {
+            "nome": "Chácara Klabin",
+            "conexões": ["Linha 5 - Lilás"]
+          },
+          {
+            "nome": "Santos-Imigrantes",
+            "conexões": []
+          },
+          {
+            "nome": "Alto do Ipiranga",
+            "conexões": []
+          },
+          {
+            "nome": "Sacomã",
+            "conexões": []
+          },
+          {
+            "nome": "Tamanduateí",
+            "conexões": ["Linha 10 - Turquesa"]
+          },
+          {
+            "nome": "Vila Prudente",
+            "conexões": ["Linha 15 - Prata"]
+          }
+        ]
+      },
+      {
+        "linha": "Linha 3 - Vermelha",
+        "estações": [
+          {
+            "nome": "Palmeiras-Barra Funda",
+            "conexões": ["Linha 7 - Rubi", "Linha 8 - Diamante"]
+          },
+          {
+            "nome": "Marechal Deodoro",
+            "conexões": []
+          },
+          {
+            "nome": "Santa Cecília",
+            "conexões": []
+          },
+          {
+            "nome": "República",
+            "conexões": ["Linha 4 - Amarela"]
+          },
+          {
+            "nome": "Anhangabaú",
+            "conexões": []
+          },
+          {
+            "nome": "Sé",
+            "conexões": ["Linha 1 - Azul"]
+          },
+          {
+            "nome": "Pedro II",
+            "conexões": []
+          },
+          {
+            "nome": "Brás",
+            "conexões": ["Linha 10 - Turquesa", "Linha 11 - Coral", "Linha 12 - Safira"]
+          },
+          {
+            "nome": "Bresser-Mooca",
+            "conexões": []
+          },
+          {
+            "nome": "Belém",
+            "conexões": []
+          },
+          {
+            "nome": "Tatuapé",
+            "conexões": ["Linha 11 - Coral", "Linha 12 - Safira"]
+          },
+          {
+            "nome": "Carrão",
+            "conexões": []
+          },
+          {
+            "nome": "Penha",
+            "conexões": []
+          },
+          {
+            "nome": "Vila Matilde",
+            "conexões": []
+          },
+          {
+            "nome": "Guilhermina-Esperança",
+            "conexões": []
+          },
+          {
+            "nome": "Patriarca",
+            "conexões": []
+          },
+          {
+            "nome": "Artur Alvim",
+            "conexões": []
+          },
+          {
+            "nome": "Corinthians-Itaquera",
+            "conexões": ["Linha 11 - Coral"]
+          }
+        ]
+      },
+      {
+        "linha": "Linha 4 - Amarela",
+        "estações": [
+          {
+            "nome": "Luz",
+            "conexões": ["Linha 1 - Azul", "Linha 7 - Rubi", "Linha 11 - Coral"]
+          },
+          {
+            "nome": "República",
+            "conexões": ["Linha 3 - Vermelha"]
+          },
+          {
+            "nome": "Higienópolis-Mackenzie",
+            "conexões": []
+          },
+          {
+            "nome": "Paulista",
+            "conexões": ["Linha 2 - Verde"]
+          },
+          {
+            "nome": "Fradique Coutinho",
+            "conexões": []
+          },
+          {
+            "nome": "Faria Lima",
+            "conexões": []
+          },
+          {
+            "nome": "Pinheiros",
+            "conexões": ["Linha 9 - Esmeralda"]
+          },
+          {
+            "nome": "Butantã",
+            "conexões": []
+          },
+          {
+            "nome": "São Paulo-Morumbi",
+            "conexões": []
+          },
+          {
+            "nome": "Vila Sônia",
+            "conexões": []
+          }
+        ]
+      },
+      {
+        "linha": "Linha 5 - Lilás",
+        "estações": [
+          {
+            "nome": "Capão Redondo",
+            "conexões": []
+          },
+          {
+            "nome": "Campo Limpo",
+            "conexões": []
+          },
+          {
+            "nome": "Vila das Belezas",
+            "conexões": []
+          },
+          {
+            "nome": "Giovanni Gronchi",
+            "conexões": []
+          },
+          {
+            "nome": "Santo Amaro",
+            "conexões": ["Linha 9 - Esmeralda"]
+          },
+          {
+            "nome": "Largo Treze",
+            "conexões": []
+          },
+          {
+            "nome": "Adolfo Pinheiro",
+            "conexões": []
+          },
+          {
+            "nome": "Alto da Boa Vista",
+            "conexões": []
+          },
+          {
+            "nome": "Borba Gato",
+            "conexões": []
+          },
+          {
+            "nome": "Brooklin",
+            "conexões": []
+          },
+          {
+            "nome": "Campo Belo",
+            "conexões": []
+          },
+          {
+            "nome": "Eucaliptos",
+            "conexões": []
+          },
+          {
+            "nome": "Moema",
+            "conexões": []
+          },
+          {
+            "nome": "AACD-Servidor",
+            "conexões": []
+          },
+          {
+            "nome": "Hospital São Paulo",
+            "conexões": []
+          },
+          {
+            "nome": "Chácara Klabin",
+            "conexões": ["Linha 2 - Verde"]
+          }
+        ]
+      },
+      {
